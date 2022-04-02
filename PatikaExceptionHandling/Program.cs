f76b1674a11d4ddc24a9f4db5604127e4c157c47
Emre Pirvan <emreprvn@gmail.com>
2022-04-02 04:44:04 +0300

    Add project files.

---
+++ PatikaExceptionHandling/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace PatikaExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {       ///hata alma ihtimali olan kod bloğunu giriyoruz
+                Console.WriteLine("Bir sayı giriniz : ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("girmiş olduğunuz sayi :"  + sayi);
+            }
+            catch (Exception ex)
+            { //hatayı yakaladığımızda yapılacak şeyler
+                Console.WriteLine("Hata :  "+ex.Message.ToString());
+                
+            }
+            finally
+            {   //hata alsakta almmasakta çalışan yer (zorunlu değil)
+                Console.WriteLine("işlem tamamlandı.");
+            }
+            Console.WriteLine("===========================================");
+            try
+            {
+            //    int a = int.Parse("test");
+                int a = int.Parse("-200000000000");
+            }
+            catch (OverflowException ex)
+            //catch(ArgumentNullException ex)
+            //catch (FormatException ex)
+            {
+                //Console.WriteLine("Veri TİPİ UYGUN DEĞİL");
+                Console.WriteLine("ÇOK Küçük ya da çok büyük bir değer girdiniz");
+
+                Console.WriteLine(ex);
+            }
+            finally
+            {  
+                Console.WriteLine("işlem tamamlandı.");
+            }
+        }
+    }
+}
